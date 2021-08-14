324a0511d14c9ea1ecb13f24a4ec0e68dac69c49
Lenoir Seibert <siwebbr@gmail.com>
2021-08-14 07:05:02 -0300

    End Day 13 Js

--- 13-Random-Choice-Picker/script.js
+++ 13-Random-Choice-Picker/script.js
@@ -5,6 +5,13 @@ textarea.focus()
 
 textarea.addEventListener('keyup', (event) => {
   createTags(event.target.value)
+
+  if (event.key === 'Enter') {
+    setTimeout(() => {
+      event.target.value = ''
+    }, 10)
+    randomSelect()
+  }
 })
 
 function createTags(input) {
@@ -21,4 +28,36 @@ function createTags(input) {
     tagEl.innerText = tag
     tagsEl.appendChild(tagEl)
   })
+}
+
+function randomSelect() {
+  const times = 30
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag()
+    highlightTag(randomTag)
+    setTimeout(() => {
+      unhighlightTag(randomTag)
+    }, 100)
+  }, 100)
+  setTimeout(() => {
+    clearInterval(interval)
+    setTimeout(() => {
+      const randomTag = pickRandomTag()
+      highlightTag(randomTag)
+    }, 100)
+  }, times * 100)
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag')
+  return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+  tag.classList.add('highlight')
+}
+
+function unhighlightTag(tag) {
+  tag.classList.remove('highlight')
 }
